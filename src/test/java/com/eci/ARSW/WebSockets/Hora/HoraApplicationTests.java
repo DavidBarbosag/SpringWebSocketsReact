64d9e3d044177ced6f12b910b18723b2a70eec5b
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-07-03 11:50:03 -0500

    Commit inicial

---
+++ src/test/java/com/eci/ARSW/WebSockets/Hora/HoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.WebSockets.Hora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
